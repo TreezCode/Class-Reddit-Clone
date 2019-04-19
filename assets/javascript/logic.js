71338760bcc358f3904b208193b71f85dac6947c
Kyle Knox <kyleknox86@gmail.com>
2019-04-18 18:42:45 -0600

    additions

--- assets/javascript/logic.js
+++ assets/javascript/logic.js
@@ -1,38 +1,34 @@
 $(document).ready(function () {
-    var createdAt = new Date();
-    var dummyData = [{
-            title: 'Funny Video!',
-            link: 'https://www.youtube.com',
-            votes: 32,
-            user: 'Kyle',
-            createdAt: createdAt
-        },
-        {
-            title: 'Funny Video!',
-            link: 'https://www.youtube.com',
-            votes: 32,
-            user: 'Kyle',
-            createdAt: createdAt
-        },
-        {
-            title: 'Funny Video!',
-            link: 'https://www.youtube.com',
-            votes: 32,
-            user: 'Kyle',
-            createdAt: createdAt
-        }
-    ];
+    // Initialize Firebase
+    var config = {
+        apiKey: "AIzaSyAth4J3q0fA2kZoJ0mrsWZsORGv1szSSDM",
+        authDomain: "class-reddit.firebaseapp.com",
+        databaseURL: "https://class-reddit.firebaseio.com",
+        projectId: "class-reddit",
+        storageBucket: "class-reddit.appspot.com",
+        messagingSenderId: "240036141571"
+    };
+    firebase.initializeApp(config);
+    var database = firebase.database();
 
-    dummyData.forEach(function (item) {
+    database.ref('/items').once('value').then(function (snapshot) {
+        var results = snapshot.val();
+        for (var id in results) {
+            buildItemElement(results[id]);
+        }
+    })
+
+
+    function buildItemElement(item) {
         var $template = $('#content-template').clone();
         var newItem = $template.prop('content');
 
         $(newItem).find('.content-title').text(item.title);
         $(newItem).find('.votes').text(item.votes);
         $(newItem).find('.content-link').attr('href', item.link).attr('target', '_blank');
-        $(newItem).find('.content-meta').text(item.user + ' posted at ' + item.createdAt);
+        $(newItem).find('.content-meta').text(item.user + ' posted at ' + moment(item.createdAt).fromNow());
 
         $('#list').append(newItem);
 
-    });
+    };
 });
